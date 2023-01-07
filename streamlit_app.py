1774d1dd9123b46cc10715f269a134f90e715559
Sthon <122157901+snwflakesthon@users.noreply.github.com>
2023-01-07 07:54:45 -0500

    Update streamlit_app.py
    Added codes as per the chapter3 learning course

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 
 import streamlit
 
-streamlit.title('My DABW Learning')
-streamlit.header('streamlit app learning')
-streamlit.text('adding header, title, text')
-streamlit.text('checking for errors')
-streamlit.text('working on error')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
