f8a23c82b27eaecc2b98ed069ce123f223290a00
Sthon <122157901+snwflakesthon@users.noreply.github.com>
2023-01-07 07:51:22 -0500

    Update streamlit_app.py
    Added more lines of code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 
 import streamlit
 
-streamlit.title('My DABW Learning Next Step')
+streamlit.title('My DABW Learning')
+streamlit.header('streamlit app learning')
+streamlit.text('addinh header, title, text')
+streamlit.text('checking for errors')
+streamlit.text('working on error')
