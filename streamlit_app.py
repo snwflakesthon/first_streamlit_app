a458231cb6ab1c0826a31a41ee11b5e576438d7d
Sthon <122157901+snwflakesthon@users.noreply.github.com>
2023-01-07 07:51:52 -0500

    Update streamlit_app.py
    corrected the spelling

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 
 streamlit.title('My DABW Learning')
 streamlit.header('streamlit app learning')
-streamlit.text('addinh header, title, text')
+streamlit.text('adding header, title, text')
 streamlit.text('checking for errors')
 streamlit.text('working on error')
