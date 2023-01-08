c7bef33eeb464b43c4357f30a4588878d0dcaa1f
Sthon <122157901+snwflakesthon@users.noreply.github.com>
2023-01-08 09:01:52 -0500

    Kiwi Display

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,10 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response)
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
 
 # write your own comment -what does the next line do? 
