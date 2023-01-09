43351dc7c870ef1589496bd317a6bde8913febc1
Sthon <122157901+snwflakesthon@users.noreply.github.com>
2023-01-09 10:28:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,18 +36,15 @@ import requests
 try:
   fruit_choice = streamlit.text.input('What fruit would you like information about?')
   if not fruit_choice:
-    streamlit.error("Please select a fruit to get information")
+    streamlit.error("Please select a fruit to get information.")
     else:
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response)
 #streamlit.text(fruityvice_response.json())
-
-
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-
 except URLError as e:
   streamlit.error()
   
