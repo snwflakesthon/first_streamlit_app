7d44bcf42eebf5d8147b5f5661b51b44a085f57c
Sthon <122157901+snwflakesthon@users.noreply.github.com>
2023-01-10 11:00:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,6 +81,6 @@ if streamlit.button('Get Fruit Load List'):
 #add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 #streamlit.write('Thanks for entering ', add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 
